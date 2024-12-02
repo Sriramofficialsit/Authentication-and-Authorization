0480a0da6658eaca3fdbe14d112e0370c7a3f9a6
SRIRAM P <165500350+Sriramofficialsit@users.noreply.github.com>
2024-12-02 21:35:05 +0530

    Add files via upload

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 async function dbconnect(){
     
-    // const mongodb_uri = process.env.MONGODB_URI;
-    const mongodb_uri = "mongodb://127.0.0.1:27017/recipes";
+    const mongodb_uri = process.env.MONGODB_URI;
+    // const mongodb_uri = "mongodb://127.0.0.1:27017/recipes";
     try{
         await mongoose.connect(mongodb_uri);
         console.log("DB connected sucessfully");
